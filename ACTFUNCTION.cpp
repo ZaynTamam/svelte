d416d6cc5b991776e21974c014a10a46ea049c6b
ZaynTamam <tamam.zain78@gmail.com>
2026-03-02 12:45:13 +0700

    Menghitung keliling persegi

---
+++ ACTFUNCTION.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+    cout << "Masukkan lebar: ";
+    cin >> lebar;
+}
